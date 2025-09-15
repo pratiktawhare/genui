83571e06da0961767a819bfe2d872491ad0a8bb3
pratiktawhare <pratiktawhare3@gmail.com>
2025-09-15 17:06:03 +0530

    updated title

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
-import NoPage from './pages/Nopage';
+import NoPage from './pages/NoPage';
 
 const App = () => {
   return (
